b50f645cbda6ba4b66459e82bcf238fcca120366
Mohisha Patel <patel.4088@osu.edu>
2024-10-19 17:36:39 -0400

    home page

--- cargoconnect/src/components/Home.js
+++ cargoconnect/src/components/Home.js
@@ -1,16 +1,35 @@
-// Home Page 
-
-import React from "react";
-import {Link} from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importing Link for client-side navigation
+import '../static/Home.css';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="/Home/BookingPage">Booking Page</Link>
-        </div>
-    );
-};
+  return (
+    <div className="home-container">
+      {/* Logo at the top */}
+      <div className="logo-container">
+        <img src="/cargoLogo.png" alt="Logo" className="logo" />
+      </div>
+
+      {/* Navigation Bar */}
+      <nav className="nav-bar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services">Services</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default Home;
+
